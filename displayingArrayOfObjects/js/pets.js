c4f848b0b778f67260e9a0c69d25dfba99794c8f
joshhofstad <50232510+joshhofstad@users.noreply.github.com>
2020-08-06 20:21:14 +0200

    Add files via upload

---
+++ displayingArrayOfObjects/js/pets.js
@@ -0,0 +1,39 @@
+// Array of 'pet' objects
+
+const pets = [
+  {
+    name: 'Brutus',
+    type: 'Dog',
+    breed: 'Australian Shepherd',
+    age: 8,
+    photo: 'img/aussie.jpg'
+  },
+  { 
+    name: 'Patches',
+    type: 'Cat',
+    breed: 'Domestic Shorthair',
+    age: 1,
+    photo: 'img/tabby.jpg'
+  },
+  { 
+    name: 'Titus',
+    type: 'Dog',
+    breed: 'Pug',
+    age: 6,
+    photo: 'img/pug.jpg'
+  },
+  { 
+    name: 'Rory',
+    type: 'Cat',
+    breed: 'Persian',
+    age: 5,
+    photo: 'img/persian.jpg'
+  },
+  { 
+    name: 'Ottis',
+    type: 'Dog',
+    breed: 'Golden Retriever',
+    age: 3,
+    photo: 'img/golden.jpg'
+  }
+];
